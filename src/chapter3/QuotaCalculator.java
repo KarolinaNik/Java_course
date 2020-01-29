a25d797854b147ad35c005e7ae5f6ef958c8193e
AlexandraIlianova <alexandra.ilianova@flextrade.com>
2020-01-29 15:38:31 +0000

    Java training (TAU), chapter 3

---
+++ src/chapter3/QuotaCalculator.java
@@ -0,0 +1,30 @@
+package chapter3;
+
+import java.util.Scanner;
+
+/*
+ * IF ELSE
+ * All salespeople are expected to make at least 10 sales each week.
+ * For those who do, they receive a congratulatory message.
+ * For those who don't, they are informed of how many sales they were short.
+ */
+public class QuotaCalculator {
+    public static void main(String args[]) {
+        //Initialize values we know
+        int quota = 10;
+
+        //Gen known values
+        System.out.println("Enter the number of sales this week:");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        //Make a decision
+        if (sales >= quota) {
+            System.out.println("Congrats! You've met your quota");
+        } else {
+            int salesShort = quota - sales;
+            System.out.println("You did not make your quota. You were " + salesShort + "sales short.");
+        }
+    }
+}
