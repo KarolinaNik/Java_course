8746f551494269c96dfea9234d18464d1cd769ba
AlexandraIlianova <alexandra.ilianova@flextrade.com>
2020-02-03 15:22:20 +0000

    Java training (TAU), chapter 4.

---
+++ src/chapter4/AddNumbers.java
@@ -0,0 +1,34 @@
+package chapter4;
+
+/*
+ * DO WHILE LOOP:
+ * Write a program that allows a user to enter two numbers,
+ * and then sums up the two numbers. The user should be able to
+ * repeat this action until they indicate they are done.
+ */
+
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean again = false;
+
+        do {
+            System.out.println("Enter the 1st number:");
+            double num1 = scanner.nextDouble();
+
+            System.out.println("Enter the 2nd number:");
+            double num2 = scanner.nextDouble();
+
+            double sum = num1 + num2;
+            System.out.println("The sum is " + sum);
+            System.out.println("Would you like to start over?");
+            again = scanner.nextBoolean();
+
+        } while (again);
+        scanner.close();
+    }
+}
